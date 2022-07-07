c97ae707b72f8b967f778f26c5b375e6b4b01108
Andrew <a9507652513@gmail.com>
2022-07-07 23:42:36 +0300

    with minutes

--- main.py
+++ main.py
@@ -13,15 +13,50 @@ for line in lines:
         color = 'green'
     elif 995<=daays%1000<1000:
         color = 'blue'
-    elif 85<=daays%100<100:
+    elif 85<=daays%100<=100:
         color = 'yellow'
     elif 1<=daays%100<=3:
         color = 'red'
     else:
         color = 'white'
+    cprint(f'{line}  прожил(а) уже {daays:,} дней ', color, end='')
 
-    cprint(f'{line}  прожил(а) уже {daays}',color)
+    hourz = daays * 24
+    strd=''
+    if hourz%1000<=16 or 991<=hourz%1000:
+        color = 'green'
+    elif 930<=hourz%1000<=990:
+        color = 'blue'
+        strd= str(round((1000-hourz%1000)/24))
+        strd=f'{strd} дней'
+    elif 850<=hourz%1000<=929:
+        color = 'yellow'
+        strd= str(round((1000-hourz%1000)/24))
+        strd = f'({strd} дней)'
 
+    elif 1<=hourz%1000<=10:
+        color = 'red'
+    else:
+        color = 'white'
+    cprint(f'   или {hourz:,} часов {strd}  ', color,end='')
+
+    strm=''
+    minutes = hourz * 60
+    if minutes%100000<=7000 or 99300<=minutes%100000:
+        color = 'green'
+    elif 97000<=minutes%100000<=99999:
+        color = 'blue'
+        strm = str(round((100000 - minutes%100000) / 24/60))
+        strm = f'({strm} дней)'
+    elif 93000<=minutes%100000<=97000:
+        color = 'yellow'
+        strm = str(round((100000 - minutes%100000) / 24/60))
+        strm = f'({strm} дней)'
+    elif 100<=minutes%100000<=1000:
+        color = 'red'
+    else:
+        color = 'white'
+    cprint(f' или {minutes:,} минут {strm}', color)
 
 # def out_red(text):
 #     print("\033[31m {}" .format(text))
